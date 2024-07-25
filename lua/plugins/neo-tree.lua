96590248d3663ab8c201d4f75dec19ee1db3f6c3
SajinShrestha2003 <shresthasajin59@gmail.com>
2024-07-25 22:22:10 +0545

    neo-tree updated to show hidden-files

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -1,13 +1,35 @@
+-- return {
+--	"nvim-neo-tree/neo-tree.nvim",
+--	branch = "v3.x",
+--	dependencies = {
+--		"nvim-lua/plenary.nvim",
+--		"nvim-tree/nvim-web-devicons",
+--		"MunifTanjim/nui.nvim",
+--	},
+--	config = function()
+--		vim.keymap.set("n", "<C-n>", ":Neotree filesystem reveal left toggle<CR>", {})
+--		vim.keymap.set("n", "<leader>bf", ":Neotree buffers reveal float<CR>", {})
+--	end,
+-- }
+
 return {
-	"nvim-neo-tree/neo-tree.nvim",
-	branch = "v3.x",
-	dependencies = {
-		"nvim-lua/plenary.nvim",
-		"nvim-tree/nvim-web-devicons",
-		"MunifTanjim/nui.nvim",
-	},
-	config = function()
-		vim.keymap.set("n", "<C-n>", ":Neotree filesystem reveal left toggle<CR>", {})
-		vim.keymap.set("n", "<leader>bf", ":Neotree buffers reveal float<CR>", {})
-	end,
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
+    dependencies = {
+        "nvim-lua/plenary.nvim",
+        "nvim-tree/nvim-web-devicons",
+        "MunifTanjim/nui.nvim",
+    },
+    config = function()
+        require("neo-tree").setup({
+            filesystem = {
+                filtered_items = {
+                    hide_dotfiles = false,  -- Show hidden files
+                },
+            },
+        })
+        vim.keymap.set("n", "<C-n>", ":Neotree filesystem reveal left toggle<CR>", {})
+        vim.keymap.set("n", "<leader>bf", ":Neotree buffers reveal float<CR>", {})
+    end,
 }
+
