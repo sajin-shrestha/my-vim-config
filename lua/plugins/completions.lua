a78e5c5925a3f939c28619bf0ee75c393ebf59f0
SajinShrestha2003 <shresthasajin59@gmail.com>
2024-07-25 12:47:59 +0545

    auto-completion lua-file updated

--- lua/plugins/completions.lua
+++ lua/plugins/completions.lua
@@ -1,6 +1,51 @@
+-- return {
+--   {
+--     "hrsh7th/cmp-nvim-lsp"
+--   },
+--   {
+--     "L3MON4D3/LuaSnip",
+--     dependencies = {
+--       "saadparwaiz1/cmp_luasnip",
+--       "rafamadriz/friendly-snippets",
+--     },
+--   },
+--   {
+--     "hrsh7th/nvim-cmp",
+--     config = function()
+--       local cmp = require("cmp")
+--       require("luasnip.loaders.from_vscode").lazy_load()
+
+--       cmp.setup({
+--         snippet = {
+--           expand = function(args)
+--             require("luasnip").lsp_expand(args.body)
+--           end,
+--         },
+--         window = {
+--           completion = cmp.config.window.bordered(),
+--           documentation = cmp.config.window.bordered(),
+--         },
+--         mapping = cmp.mapping.preset.insert({
+--           ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+--           ["<C-f>"] = cmp.mapping.scroll_docs(4),
+--           ["<C-Space>"] = cmp.mapping.complete(),
+--           ["<C-e>"] = cmp.mapping.abort(),
+--           ["<CR>"] = cmp.mapping.confirm({ select = true }),
+--         }),
+--         sources = cmp.config.sources({
+--           { name = "nvim_lsp" },
+--           { name = "luasnip" }, -- For luasnip users.
+--         }, {
+--           { name = "buffer" },
+--         }),
+--       })
+--     end,
+--   },
+-- }
+
 return {
   {
-    "hrsh7th/cmp-nvim-lsp"
+    "hrsh7th/cmp-nvim-lsp",
   },
   {
     "L3MON4D3/LuaSnip",
@@ -35,10 +80,61 @@ return {
         sources = cmp.config.sources({
           { name = "nvim_lsp" },
           { name = "luasnip" }, -- For luasnip users.
+          { name = "buffer" }, -- Buffer source for text in current buffer
+          { name = "path" }, -- File paths
+          { name = "nvim_lua" }, -- Neovim's Lua API
+          { name = "spell" }, -- Spelling suggestions
+          -- { name = "emoji" }, -- Emoji completion
+          -- { name = "calc" }, -- Calculator
+          -- { name = "treesitter" }, -- Treesitter source
         }, {
           { name = "buffer" },
         }),
+        formatting = {
+          format = function(entry, vim_item)
+            -- Fancy icons and a name of kind
+            vim_item.kind = require("lspkind").presets.default[vim_item.kind] .. " " .. vim_item.kind
+
+            -- Set a name for each source
+            vim_item.menu = ({
+              nvim_lsp = "[LSP]",
+              luasnip = "[LuaSnip]",
+              buffer = "[Buffer]",
+              path = "[Path]",
+              nvim_lua = "[Lua]",
+              spell = "[Spell]",
+              -- emoji = "[Emoji]",
+              -- calc = "[Calc]",
+              -- treesitter = "[Treesitter]",
+            })[entry.source.name]
+            return vim_item
+          end,
+        },
       })
     end,
   },
+  {
+    "onsails/lspkind-nvim",
+  },
+  {
+    "hrsh7th/cmp-buffer",
+  },
+  {
+    "hrsh7th/cmp-path",
+  },
+  {
+    "hrsh7th/cmp-nvim-lua",
+  },
+  {
+    "f3fora/cmp-spell",
+  },
+  -- {
+  --   "hrsh7th/cmp-emoji",
+  -- },
+  -- {
+  --   "hrsh7th/cmp-calc",
+  -- },
+  -- {
+  --   "ray-x/cmp-treesitter",
+  -- },
 }
