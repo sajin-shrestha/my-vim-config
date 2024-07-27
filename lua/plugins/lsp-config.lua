ffd2a626d9d27b98745727b24ca9abb30820753f
SajinShrestha2003 <shresthasajin59@gmail.com>
2024-07-27 16:40:57 +0545

    neo-tree customized and screen-error-message added

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -20,23 +20,27 @@
 --       local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
 --       local lspconfig = require("lspconfig")
---       lspconfig.tsserver.setup({
---         capabilities = capabilities
---       })
---       lspconfig.solargraph.setup({
---         capabilities = capabilities
---       })
---       lspconfig.html.setup({
---         capabilities = capabilities
---       })
---       lspconfig.lua_ls.setup({
---         capabilities = capabilities
---       })
+--       local servers = { "tsserver", "solargraph", "html", "lua_ls", "clangd", "gopls", "eslint"}
 
---       vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
---       vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
---       vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
---       vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
+--       for _, server in ipairs(servers) do
+--         local ok, err = pcall(function()
+--           lspconfig[server].setup({
+--             capabilities = capabilities,
+--             on_attach = function(client, bufnr)
+--               local opts = { noremap = true, silent = true, buffer = bufnr }
+--               vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+--               vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, opts)
+--               vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, opts)
+--               vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts)
+--               vim.keymap.set("n", "<Esc>", "<C-o>", opts)
+--             end,
+--           })
+--         end)
+
+--         if not ok then
+--           vim.notify("Error setting up LSP for " .. server .. ": " .. err, vim.log.levels.ERROR)
+--         end
+--       end
 --     end,
 --   },
 -- }
@@ -63,21 +67,54 @@ return {
       local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
       local lspconfig = require("lspconfig")
-      local servers = { "tsserver", "solargraph", "html", "lua_ls", "clangd", "gopls" }
+      local servers = { "tsserver", "solargraph", "html", "lua_ls", "clangd", "gopls", "eslint" }
+
+      local notify_error = function(msg)
+        vim.notify(msg, vim.log.levels.ERROR, { title = "LSP Error", timeout = 5000 })
+      end
+
+      local custom_on_attach = function(client, bufnr)
+        local opts = { noremap = true, silent = true, buffer = bufnr }
+        vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+        vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, opts)
+        vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, opts)
+        vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts)
+        vim.keymap.set("n", "<Esc>", "<C-o>", opts)
+      end
 
       for _, server in ipairs(servers) do
-        lspconfig[server].setup({
-          capabilities = capabilities,
-          on_attach = function(client, bufnr)
-            local opts = { noremap = true, silent = true, buffer = bufnr }
-            vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
-            vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, opts)
-            vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, opts)
-            vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts)
-            vim.keymap.set("n", "<Esc>", "<C-o>", opts)
-          end,
-        })
+        local ok, err = pcall(function()
+          lspconfig[server].setup({
+            capabilities = capabilities,
+            on_attach = custom_on_attach,
+          })
+        end)
+
+        if not ok then
+          notify_error("Error setting up LSP for " .. server .. ": " .. err)
+        end
       end
+
+      vim.diagnostic.config({
+        virtual_text = {
+          severity = { min = vim.diagnostic.severity.ERROR },
+          format = function(diagnostic)
+            return string.format("Error: %s", diagnostic.message)
+          end,
+        },
+        signs = true,
+        underline = true,
+        update_in_insert = true,
+        severity_sort = true,
+        float = {
+          source = "always",
+          border = "rounded",
+          focusable = false,
+        },
+      })
+
+      -- Set the highlight group for diagnostic errors
+      vim.api.nvim_set_hl(0, "DiagnosticVirtualTextError", { fg = "#FF0000", bold = true, italic = true })
     end,
   },
 }
--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -1,15 +1,22 @@
 -- return {
---	"nvim-neo-tree/neo-tree.nvim",
---	branch = "v3.x",
---	dependencies = {
---		"nvim-lua/plenary.nvim",
---		"nvim-tree/nvim-web-devicons",
---		"MunifTanjim/nui.nvim",
---	},
---	config = function()
---		vim.keymap.set("n", "<C-n>", ":Neotree filesystem reveal left toggle<CR>", {})
---		vim.keymap.set("n", "<leader>bf", ":Neotree buffers reveal float<CR>", {})
---	end,
+--     "nvim-neo-tree/neo-tree.nvim",
+--     branch = "v3.x",
+--     dependencies = {
+--         "nvim-lua/plenary.nvim",
+--         "nvim-tree/nvim-web-devicons",
+--         "MunifTanjim/nui.nvim",
+--     },
+--     config = function()
+--         require("neo-tree").setup({
+--             filesystem = {
+--                 filtered_items = {
+--                     hide_dotfiles = false,  -- Show hidden files
+--                 },
+--             },
+--         })
+--         vim.keymap.set("n", "<C-n>", ":Neotree filesystem reveal left toggle<CR>", {})
+--         vim.keymap.set("n", "<leader>bf", ":Neotree buffers reveal float<CR>", {})
+--     end,
 -- }
 
 return {
@@ -26,10 +33,17 @@ return {
                 filtered_items = {
                     hide_dotfiles = false,  -- Show hidden files
                 },
+                window = {
+                    width = 30,  -- Decrease the width
+                },
+            },
+            buffers = {
+                window = {
+                    width = 30,  -- Decrease the width for buffers as well
+                },
             },
         })
         vim.keymap.set("n", "<C-n>", ":Neotree filesystem reveal left toggle<CR>", {})
         vim.keymap.set("n", "<leader>bf", ":Neotree buffers reveal float<CR>", {})
     end,
 }
-
