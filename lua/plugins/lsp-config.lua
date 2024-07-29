9162721e5b26359f1179eacb6a3b45904c502015
SajinShrestha2003 <shresthasajin59@gmail.com>
2024-07-29 08:34:11 +0545

    lsp-config updated

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,50 +1,3 @@
--- return {
---   {
---     "williamboman/mason.nvim",
---     lazy = false,
---     config = function()
---       require("mason").setup()
---     end,
---   },
---   {
---     "williamboman/mason-lspconfig.nvim",
---     lazy = false,
---     opts = {
---       auto_install = true,
---     },
---   },
---   {
---     "neovim/nvim-lspconfig",
---     lazy = false,
---     config = function()
---       local capabilities = require('cmp_nvim_lsp').default_capabilities()
-
---       local lspconfig = require("lspconfig")
---       local servers = { "tsserver", "solargraph", "html", "lua_ls", "clangd", "gopls", "eslint"}
-
---       for _, server in ipairs(servers) do
---         local ok, err = pcall(function()
---           lspconfig[server].setup({
---             capabilities = capabilities,
---             on_attach = function(client, bufnr)
---               local opts = { noremap = true, silent = true, buffer = bufnr }
---               vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
---               vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, opts)
---               vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, opts)
---               vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts)
---               vim.keymap.set("n", "<Esc>", "<C-o>", opts)
---             end,
---           })
---         end)
-
---         if not ok then
---           vim.notify("Error setting up LSP for " .. server .. ": " .. err, vim.log.levels.ERROR)
---         end
---       end
---     end,
---   },
--- }
-
 return {
   {
     "williamboman/mason.nvim",
@@ -118,4 +71,3 @@ return {
     end,
   },
 }
-
