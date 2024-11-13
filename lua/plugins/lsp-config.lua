d6c44badf6940062cfa380c2e3796e2f785a8789
nomadDEV <shresthasajin59@gmail.com>
2024-11-13 22:21:42 +0545

    .NET language configuration added

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,3 +1,77 @@
+-- return {
+--   {
+--     "williamboman/mason.nvim",
+--     lazy = false,
+--     config = function()
+--       require("mason").setup()
+--     end,
+--   },
+--   {
+--     "williamboman/mason-lspconfig.nvim",
+--     lazy = false,
+--     opts = {
+--       auto_install = true,
+--     },
+--   },
+--   {
+--     "neovim/nvim-lspconfig",
+--     lazy = false,
+--     config = function()
+--       local capabilities = require('cmp_nvim_lsp').default_capabilities()
+
+--       local lspconfig = require("lspconfig")
+--       local servers = { "tsserver", "solargraph", "html", "lua_ls", "clangd", "gopls", "eslint" }
+
+--       local notify_error = function(msg)
+--         vim.notify(msg, vim.log.levels.ERROR, { title = "LSP Error", timeout = 5000 })
+--       end
+
+--       local custom_on_attach = function(client, bufnr)
+--         local opts = { noremap = true, silent = true, buffer = bufnr }
+--         vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+--         vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, opts)
+--         vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, opts)
+--         vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts)
+--         vim.keymap.set("n", "<Esc>", "<C-o>", opts)
+--       end
+
+--       for _, server in ipairs(servers) do
+--         local ok, err = pcall(function()
+--           lspconfig[server].setup({
+--             capabilities = capabilities,
+--             on_attach = custom_on_attach,
+--           })
+--         end)
+
+--         if not ok then
+--           notify_error("Error setting up LSP for " .. server .. ": " .. err)
+--         end
+--       end
+
+--       vim.diagnostic.config({
+--         virtual_text = {
+--           severity = { min = vim.diagnostic.severity.ERROR },
+--           format = function(diagnostic)
+--             return string.format("Error: %s", diagnostic.message)
+--           end,
+--         },
+--         signs = true,
+--         underline = true,
+--         update_in_insert = true,
+--         severity_sort = true,
+--         float = {
+--           source = "always",
+--           border = "rounded",
+--           focusable = false,
+--         },
+--       })
+
+--       -- Set the highlight group for diagnostic errors
+--       vim.api.nvim_set_hl(0, "DiagnosticVirtualTextError", { fg = "#FF0000", bold = true, italic = true })
+--     end,
+--   },
+-- }
+
 return {
   {
     "williamboman/mason.nvim",
@@ -20,7 +94,7 @@ return {
       local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
       local lspconfig = require("lspconfig")
-      local servers = { "tsserver", "solargraph", "html", "lua_ls", "clangd", "gopls", "eslint" }
+      local servers = { "tsserver", "solargraph", "html", "lua_ls", "clangd", "gopls", "eslint", "omnisharp" }
 
       local notify_error = function(msg)
         vim.notify(msg, vim.log.levels.ERROR, { title = "LSP Error", timeout = 5000 })
@@ -37,10 +111,20 @@ return {
 
       for _, server in ipairs(servers) do
         local ok, err = pcall(function()
-          lspconfig[server].setup({
-            capabilities = capabilities,
-            on_attach = custom_on_attach,
-          })
+          -- Special configuration for Omnisharp
+          if server == "omnisharp" then
+            lspconfig.omnisharp.setup({
+              capabilities = capabilities,
+              on_attach = custom_on_attach,
+              cmd = { "omnisharp", "--languageserver", "--hostPID", tostring(vim.fn.getpid()) },
+              root_dir = lspconfig.util.root_pattern(".git", "*.sln"),
+            })
+          else
+            lspconfig[server].setup({
+              capabilities = capabilities,
+              on_attach = custom_on_attach,
+            })
+          end
         end)
 
         if not ok then
